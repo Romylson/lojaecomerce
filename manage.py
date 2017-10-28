ad1eb7b4e44bc9880da20d46dfe53dd2af0d41fe
Romylson <romylson98@gmail.com>
2017-10-28 18:35:50 -0300

    resolvendo erros

--- manage.py
+++ manage.py
@@ -1,21 +1,9 @@
 #!/usr/bin/env python
-#import os
-#import sys
-#reload(sys);
-#sys.setdefaultencoding("utf8")
-
-#if __name__ == "__main__":
- #   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lojaecomerce.settings")
-
-  #  from django.core.management import execute_from_command_line
-
-   # execute_from_command_line(sys.argv)
-
-#!/usr/bin/env python
-from importlib import reload
+# encoding=utf8
 import os
 import sys
-reload(sys);
+reload(sys)
+sys.setdefaultencoding("utf8")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lojaecomerce.settings")
