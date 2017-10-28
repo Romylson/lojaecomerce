967c9524eba827484b72276b6ea0cdac320f9b44
Romylson <romylson98@gmail.com>
2017-10-28 18:18:21 -0300

    resolvendo erros

--- manage.py
+++ manage.py
@@ -16,7 +16,6 @@ from importlib import reload
 import os
 import sys
 reload(sys);
-sys.setdefaultencoding("utf8")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lojaecomerce.settings")
