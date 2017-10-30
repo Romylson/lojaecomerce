c702d98201a094e743e8260aa1025c2b7ac0b1d1
Romylson <romylson98@gmail.com>
2017-10-30 00:52:16 -0300

    erros

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/romylson/projetos/lojaecomerce/venv"
+set -gx VIRTUAL_ENV "/home/romylson/projetos/lojaecomerce"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
