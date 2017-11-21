3908b50a76a31519f261fd144151aafe664c2a1a
Romylson <romylson98@gmail.com>
2017-11-21 01:17:53 -0300

    aula49

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 from django.shortcuts import render
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -11,6 +11,7 @@ from .forms import UserAdminCreationForm
 class IndexView(LoginRequiredMixin, TemplateView):
 	template_name = 'accounts/index.html'
 
+
 class RegisterView(CreateView):
 
     model = User
@@ -18,5 +19,16 @@ class RegisterView(CreateView):
     form_class = UserAdminCreationForm
     success_url = reverse_lazy('index')
 
+class UpdateUserView(LoginRequiredMixin, UpdateView):
+ 
+     model = User
+     template_name = 'accounts/update_user.html'
+     fields = ['name', 'email']
+     success_url = reverse_lazy('accounts:index')
+ 
+     def get_object(self):
+        return self.request.user
+
 index = IndexView.as_view()
-register = RegisterView.as_view()
+update_user = UpdateUserView.as_view()
+register = RegisterView.as_view()        
